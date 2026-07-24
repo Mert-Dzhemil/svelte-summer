aa55d92efc3dc2a39aee093cac34925f8cdd845f
Mert-Dzhemil <mertdzhemil@gmail.com>
2026-07-24 14:29:50 +0300

    Fixed.
    Removed everything.
    And changed "#profile-photo" to ".avatar".

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,11 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from './assets/vite.svg'
-  import heroImg from './assets/hero.png'
-  import Counter from './lib/Counter.svelte'
+<script> 
   import ProfileCard from './ProfileCard.svelte'
 </script>
 
 <section id="center">
-  <div class="hero">
-    <img src={heroImg} class="base" width="170" height="179" alt="" />
-    <img src={svelteLogo} class="framework" alt="Svelte logo" />
-    <img src={viteLogo} class="vite" alt="Vite logo" />
-  </div>
   <div>
     <h1>MertDzhemil</h1>
-    <p>Edit <code>src/App.svelte</code> and save to test <code>HMR</code></p>
   </div>
-  <Counter />
   <ProfileCard />
 </section>
 
-<div class="ticks"></div>
-
-
-<div class="ticks"></div>
-<section id="spacer"></section>
